650f46aa846c8720b3895b07438c187c12aa66ca
argszero <argszero@gmail.com>
2015-07-14 18:27:52 +0800

    doc and init api-gateway

---
+++ api-gateway/src/main/java/com/github/esz/api/gateway/App.java
@@ -0,0 +1,13 @@
+package com.github.esz.api.gateway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
